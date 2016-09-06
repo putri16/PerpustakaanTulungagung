6dbebb6d219fcd18d1647f0bbc178ba17649f3c1
Putri Aditya Pratama <putriaditya166@gmail.com>
2016-09-06 02:18:47 -0700

    menampilkan hasil checkbox buku yang dipinjam

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2031/perpustakaantulungagung/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2031/perpustakaantulungagung/MainActivity.java
@@ -3,6 +3,7 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl2031.perpustakaantulungagung;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText etNama, etTahun, etLama;
     RadioGroup rgJK;
+    CheckBox cbP, cbE, cbH, cbPD, cbA;
     TextView tvHasil;
 
     @Override
@@ -23,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
         etTahun = (EditText) findViewById(R.id.editTextTahun);
         etLama = (EditText) findViewById(R.id.editTextLama);
         rgJK = (RadioGroup) findViewById(R.id.radiogroupJK);
+        cbP = (CheckBox) findViewById(R.id.checkBoxP);
+        cbE = (CheckBox) findViewById(R.id.checkBoxE);
+        cbH = (CheckBox) findViewById(R.id.checkBoxH);
+        cbPD = (CheckBox) findViewById(R.id.checkBoxPD);
+        cbA = (CheckBox) findViewById(R.id.checkBoxA);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
         findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
@@ -44,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         builder.append(nama + "\n");
         builder.append("Anda lahir tahun " + tahun);
         builder.append(" usia anda sekarang : ");
-        builder.append(usia + "\n");
+        builder.append(usia + "tahun\n");
 
         if (rgJK.getCheckedRadioButtonId() != -1) {
             RadioButton rb = (RadioButton) findViewById(rgJK.getCheckedRadioButtonId());
@@ -52,6 +59,16 @@ public class MainActivity extends AppCompatActivity {
         }
         builder.append("Jenis kelamin anda : ");
         builder.append(hasil + "\n");
+
+        String buku = "Buku yang anda pinjam : \n";
+        int startlen = buku.length();
+        if (cbP.isChecked()) buku += cbP.getText() + "\n";
+        if (cbE.isChecked()) buku += cbE.getText() + "\n";
+        if (cbH.isChecked()) buku += cbH.getText() + "\n";
+        if (cbPD.isChecked()) buku += cbPD.getText() + "\n";
+        if (cbA.isChecked()) buku += cbA.getText() + "\n";
+        if (buku.length() == startlen) buku += "Tidak ada buku yang dipilih";
+        builder.append(buku);
 
         int lama = Integer.parseInt(etLama.getText().toString());
         int telat = lama - 3;
